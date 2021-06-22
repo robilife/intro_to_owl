bc5b10755e991e324efa2729a3ae2cecd5a17e95
robilife <mbaye.ababacar10@gmail.com>
2021-06-22 12:24:21 +0000

    FIX

--- intro_to_owl_part_1/__manifest__.py
+++ intro_to_owl_part_1/__manifest__.py
@@ -1,10 +1,10 @@
 # __manifest__.py 
 
- { 
-     "name": "Introduction to OWL in Odoo - Part 1", 
-     "summary": "Provides an example module for OWL.", 
-     "description": "Provides an example module for OWL.", 
-     "author": "Oocademy", 
+ {
+     "name": "Introduction to OWL in Odoo - Part 1",
+     "summary": "Provides an example module for OWL.",
+     "description": "Provides an example module for OWL.",
+     "author": "Oocademy",
      "website": "http://www.oocademy.com",
      "category": "Tutorials",
      "version": "14.0.0.1",
@@ -12,4 +12,4 @@
      "demo": [],
      "data": ["assets.xml"],
      "qweb": ["static/src/js/components/PartnerOrderSummary.xml"]
- } 
+ }
