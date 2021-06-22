c5805d9b2f1870984c502db8f7e1cc8fe40c3479
Robilife <odoo@eupd3.odoo.com>
2021-06-22 11:51:01 +0000

    initial commit

---
+++ intro_to_owl_part_1/static/src/js/components/PartnerOrderSummary.py
@@ -0,0 +1,11 @@
+odoo.define("intro_to_owl_part_1.PartnerOrderSummary", function (require) { 
+     const { Component } = owl; 
+  
+     class PartnerOrderSummary extends Component { 
+         // 
+     }; 
+  
+     Object.assign(PartnerOrderSummary, { 
+         template: "intro_to_owl_part_1.PartnerOrderSummary" 
+     }); 
+ });   
